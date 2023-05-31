b9937bd1539fe9d7bec63f3dc878997af6f11289
sliverKi <lovee2756@gmail.com>
2023-06-01 03:38:45 +0900

    Feat: set develop mode2.

--- config/settings.py
+++ config/settings.py
@@ -216,7 +216,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #by gunicorn
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 ELASTICSEARCH_DSL = {
     'default': {
