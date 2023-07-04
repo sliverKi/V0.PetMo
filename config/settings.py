fb52987af0e37794f284df65ab81225e40707a18
sliverKi <lovee2756@gmail.com>
2023-07-05 02:06:39 +0900

    Feat: add cors allowed origin

--- config/settings.py
+++ config/settings.py
@@ -32,12 +32,16 @@ if RENDER_EXTERNAL_HOSTNAME:
 #CORS Setting
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = [
+    "https://petmo-frontend-4tqxc2n8y-moonyerim2.vercel.app/",
+]
+
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://petmo-frontend-4tqxc2n8y-moonyerim2.vercel.app/" # :: "front-address"
+    # "https://petmo-frontend-4tqxc2n8y-moonyerim2.vercel.app/" # :: "front-address"
 ]
 CORS_ALLOW_HEADERS = [
     'accept',
